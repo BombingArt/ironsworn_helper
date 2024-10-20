413f64e3cb8b8902bcbd5b6beea5b01e96deca1c
BombingArt <yaroshenkoyarik987@gmail.com>
2024-10-20 09:06:20 +0300

    Добавлена кастомная модель юзера

---
+++ ironhelper/user/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class BaseUserManager(BaseUserManager):
+    '''Базовый класс для создания юзеров и суперюзеров'''
+
+    def create_user(self, username, email, password=None):
+        if not username:
+            raise ValueError("Users must have a username.")
+        if not email:
+            raise ValueError("Users must have an email address.")
+        if not password:
+            raise ValueError("Users must have a password.")
+
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username, email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    '''Кастомная модель юзера'''
+
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = BaseUserManager()
+
+    def __str__(self):
+        return self.username
